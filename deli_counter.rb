4dfd9c4ff07a74df1aa145b2ffe75b998a4d5fed
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-07-17 15:21:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+katz_deli = []
+
+def line(array)
+  ret = "The line is currently: "
+  if array.length == 0
+    puts "The line is currently empty."
+  else 
+    while (array.length > 0) do |i|
+      puts ret + "#{i}. #{array[i]}" + (i == array.length - 1) ? "" : (", ")
+    end
+  end
+end
+
+def take_a_number(array, name)
+
+end
+
+def now_serving
+  
+end
