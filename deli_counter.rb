11211c79066d3861f97cc887f0141bf3b90b6b32
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-07-17 15:28:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,11 @@
 katz_deli = []
 
 def line(array)
-  ret = "The line is currently: "
   if array.length == 0
     puts "The line is currently empty."
   else 
-    while (array.length > 0) do |i|
-      puts ret + "#{i}. #{array[i]}" + (i == array.length - 1) ? "" : (", ")
+    array.each.with_index do |name, i|
+      puts "The line is currently #{i}. #{name}"
     end
   end
 end
