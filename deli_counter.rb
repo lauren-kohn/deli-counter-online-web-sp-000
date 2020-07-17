96eaa15829da918c849280d25e38d2afed099e60
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-07-17 15:41:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,14 +1,17 @@
 katz_deli = []
 
 def line(array)
-  people = []
-  if array.length == 0
+  people = [] # creates an empty array
+  if array.length == 0 # conditional action for an empty line
     puts "The line is currently empty."
-  else 
-    array.each.with_index(1) do |name, i|
-      people.push("#{i}. #{name}")
+  else # conditional action for a line of any length
+    array.each.with_index(1) do |name, i| 
+      # iterates over each element and includes the index - requires .each and .with_index because .with_index allows for an optional argument about where to start numbering the list
+      people.push("#{i}. #{name}") 
+      # adds place in line and person's name to the array
     end
     puts "The line is currently: #{people.join(" ")}"
+    # outputs a string using the people array with a space between each name
   end
 end
 
